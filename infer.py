16fa50098294027b9be450a45d05bbb688ff7474
Pavel <hydrophonyx@gmail.com>
2024-01-09 20:52:29 +0300

    add mlflow

--- infer.py
+++ infer.py
@@ -1,3 +1,4 @@
+import mlflow
 import numpy as np
 import onnxruntime as rt
 from base_dataset import BaseDataset
@@ -5,18 +6,26 @@ from dvc.api import DVCFileSystem
 from sklearn.metrics import mean_squared_error, r2_score
 
 
+mlflow.set_tracking_uri("http://128.0.1.1:8080")
+mlflow.set_experiment("my_experiment")
+
+
 def infer():
-    fs = DVCFileSystem()
-    fs.repo.pull()
-    test_data = BaseDataset(30)
-    X, y = test_data.get_Xy()
-    X = X.astype(np.float32)
-    sess = rt.InferenceSession("models/model.onnx")
-    input_name = sess.get_inputs()[0].name
-    input_data = {input_name: X.reshape(-1, X.shape[1] * X.shape[2])}
-    pred = sess.run(None, input_data)[0]
-    np.savetxt("infer_output/output.csv", pred, delimiter=",", fmt="%f")
-    print(f"infer mse:{mean_squared_error(y, pred)} r2_score: {r2_score(y, pred)}")
+    with mlflow.start_run():
+        fs = DVCFileSystem()
+        fs.repo.pull()
+        test_data = BaseDataset(30)
+        X, y = test_data.get_Xy()
+        X = X.astype(np.float32)
+        sess = rt.InferenceSession("models/model.onnx")
+        input_name = sess.get_inputs()[0].name
+        input_data = {input_name: X.reshape(-1, X.shape[1] * X.shape[2])}
+        pred = sess.run(None, input_data)[0]
+        np.savetxt("infer_output/output.csv", pred, delimiter=",", fmt="%f")
+        mse = mean_squared_error(y, pred)
+        r2 = r2_score(y, pred)
+        print(f"infer mse:{mse} r2_score: {r2}")
+        mlflow.log_metrics({"mse": mse, "r2": r2})
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
+import git
 import hydra
+import mlflow
 from base_dataset import BaseDataset
 from dvc.api import DVCFileSystem
 from omegaconf import DictConfig, OmegaConf
@@ -7,22 +9,35 @@ from skl2onnx.common.data_types import FloatTensorType
 from sklearn import linear_model
 
 
+mlflow.set_tracking_uri("http://128.0.1.1:8080")
+mlflow.set_experiment("my_experiment")
+
+
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def train(cfg: DictConfig):
-    params = OmegaConf.to_container(cfg["params"])
-    fs = DVCFileSystem()
-    fs.repo.pull()
-    data = BaseDataset(29)
-    X_train, y_train = data.get_Xy()
-    model = linear_model.Ridge(**params)
-    model.fit(X_train.reshape(-1, X_train.shape[1] * X_train.shape[2]), y_train)
-    initial_type = [
-        ("float_input", FloatTensorType([None, X_train.shape[1] * X_train.shape[2]]))
-    ]
-    onnx_model = convert_sklearn(model, initial_types=initial_type)
-    with open("models/model.onnx", "wb") as f:
-        f.write(onnx_model.SerializeToString())
-    f.close()
+    with mlflow.start_run():
+        params = OmegaConf.to_container(cfg["params"])
+
+        mlflow.log_params(params)
+
+        repo = git.Repo(search_parent_directories=True)
+        git_commit_id = repo.head.object.hexsha
+        mlflow.log_param("git_commit_id", git_commit_id)
+
+        fs = DVCFileSystem()
+        fs.repo.pull()
+
+        data = BaseDataset(29)
+        X_train, y_train = data.get_Xy()
+        model = linear_model.Ridge(**params)
+        model.fit(X_train.reshape(-1, X_train.shape[1] * X_train.shape[2]), y_train)
+        initial_type = [
+            ("float_input", FloatTensorType([None, X_train.shape[1] * X_train.shape[2]]))
+        ]
+        onnx_model = convert_sklearn(model, initial_types=initial_type)
+        with open("models/model.onnx", "wb") as f:
+            f.write(onnx_model.SerializeToString())
+        f.close()
 
     # Пушить модель через апи пока не получилось
     # fs.repo.push()
